16f12edc4086f987e6e89d567c052d17cf443abb
Johnnie843 <cooperj2@g.cofc.edu>
2019-04-15 23:01:51 -0400

    Pushing CSCI 490 Lab 7

--- app/src/main/java/edu/cofc/briggs/myapplication/adapter/MessageAdapter.java
+++ app/src/main/java/edu/cofc/briggs/myapplication/adapter/MessageAdapter.java
@@ -1,4 +1,4 @@
-package edu.cofc.myapplication.adapter;
+package edu.cofc.briggs.myapplication.adapter;
 
 import android.app.Activity;
 import android.content.Context;
@@ -14,6 +14,7 @@ import java.util.List;
 
 import edu.cofc.briggs.myapplication.R;
 import edu.cofc.briggs.myapplication.model.FriendlyMessage;
+
 
 public class MessageAdapter extends ArrayAdapter<FriendlyMessage> {
 
